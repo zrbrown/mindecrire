83950a3812571475b30fd857151f70a80eab68f6
Zack Brown <zackrbrown@gmail.com>
2020-02-23 20:21:59 -0600

    removed oauth access config since it's not needed

--- src/main/java/net/eightlives/mindy/config/SecurityConfig.java
+++ src/main/java/net/eightlives/mindy/config/SecurityConfig.java
@@ -1,18 +1,8 @@
 package net.eightlives.mindy.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
-import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
-import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -28,18 +18,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/config/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
-                .oauth2Login().tokenEndpoint().accessTokenResponseClient(accessTokenResponseClient());
+                .oauth2Login();
         http.csrf()
                 .ignoringAntMatchers("/actuator/**");
-    }
-
-    private OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
-        DefaultAuthorizationCodeTokenResponseClient client = new DefaultAuthorizationCodeTokenResponseClient();
-        RestTemplate restTemplate = new RestTemplate(Arrays.asList(
-                new FormHttpMessageConverter(), new OAuth2AccessTokenResponseHttpMessageConverter()));
-        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
-        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
-        client.setRestOperations(restTemplate);
-        return client;
     }
 }
